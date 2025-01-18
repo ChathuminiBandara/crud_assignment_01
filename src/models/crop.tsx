95b7e08e24cc1df03042261ff4928a76e25785ba
ChathuminiBandara <chathubandara500@gmail.com>
2025-01-18 23:46:19 +0530

    Crop model

---
+++ src/models/crop.tsx
@@ -0,0 +1,9 @@
+export class Crop {
+    cropCode!: string;
+    commonName!: string;
+    scientificName!: string;
+    cropImage!: string; // Can be a URL or base64 string.
+    category!: string;
+    cropSeason!: string;
+    field!: string;
+}

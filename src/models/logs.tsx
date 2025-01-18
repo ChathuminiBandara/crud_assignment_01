546dadeabfe797386ffc2b259ccdd3e02623a7ef
ChathuminiBandara <chathubandara500@gmail.com>
2025-01-18 23:47:31 +0530

    Logs Pages

---
+++ src/models/logs.tsx
@@ -0,0 +1,10 @@
+export class Logs {
+    id!: number;
+    logCode!: string;
+    logDate!: string;
+    logDetails!: string;
+    observedBy!: string;
+    crop!: string;
+    staff!: string;
+    image!: string;
+}

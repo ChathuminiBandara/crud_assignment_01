b80d283fbfce74a9612ae02b84bc2b7c03181788
ChathuminiBandara <chathubandara500@gmail.com>
2025-01-18 23:47:12 +0530

    Vehicle modules

---
+++ src/models/vehicle.tsx
@@ -0,0 +1,12 @@
+export class Vehicle {
+    id!: number;
+    vehicleCode!: string;
+    licensePlate!: string;
+    vehicleCategory!: string;
+    fuelType!: string;
+    status!: string;
+    allocatedStaff!: string;
+    remarks!: string;
+    image1!: File | null;
+    image2!: File | null;
+}

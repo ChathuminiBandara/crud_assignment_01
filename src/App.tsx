98aa428581378910f84c7f67e4a8d517785fba06
ChathuminiBandara <chathubandara500@gmail.com>
2025-01-18 23:46:31 +0530

    App Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {Footer, Navbar} from "./components/layout.tsx";
+import Home from "./pages/home.tsx";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Contact from "./pages/contact.tsx";
+import Notfound from "./pages/notfound.tsx";
+import Crop from "./pages/crop.tsx";
+import Staff from "./pages/staff.tsx";
+import Field from "./pages/field.tsx";
+import Vehicle from "./pages/vehicle.tsx";
+import CropLogs from "./pages/cropLogs.tsx";
+import EquipmentManager from "./pages/equipment.tsx";
+import Profile from "./pages/profile.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+        <Navbar />
+        <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/contact" element={<Contact/>} />
+            <Route path="/crop" element={<Crop/>} />
+            <Route path="/staff" element={<Staff/>} />
+            <Route path="/field" element={<Field/>} />
+            <Route path="/vehicle" element={<Vehicle/>} />
+            <Route path="/logs" element={<CropLogs/>} />
+            <Route path="/equipment" element={<EquipmentManager/>} />
+            <Route path="/profile" element={<Profile/>} />
+            <Route path="*" element={<Notfound/>} />
+        </Routes>
+        <Footer />
+    </BrowserRouter>
   )
 }
 

e1662dc87c615fa46b59553e793998ea64dc77e1
ChathuminiBandara <chathubandara500@gmail.com>
2025-01-18 23:44:44 +0530

    Notfound Page

---
+++ src/pages/notfound.tsx
@@ -0,0 +1,10 @@
+export default function Notfound() {
+    return (
+        <>
+            <div className="container m-4">
+                <h2>Page Not Found !</h2>
+            </div>
+
+        </>
+    )
+}

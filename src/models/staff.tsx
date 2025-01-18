d9f90a80b08b69a3e98d427b2bf472c0aafd5cba
ChathuminiBandara <chathubandara500@gmail.com>
2025-01-18 23:47:53 +0530

    Staff model

---
+++ src/models/staff.tsx
@@ -0,0 +1,17 @@
+export class Staff {
+    id!: number;
+    firstName!: string;
+    lastName!: string;
+    designation!: string;
+    gender!: string;
+    joinedDate!: string;
+    dob!: string;
+    addressLine1!: string;
+    addressLine2!: string;
+    addressLine3!: string;
+    contactNo!: string;
+    role!: string;
+    field!: string;
+    vehicle!: string;
+    image!: string;
+}
